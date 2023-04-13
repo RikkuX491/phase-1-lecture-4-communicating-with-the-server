27cd67221e7b77a1231f7a058782e9375a62fdfa
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-13 17:12:29 -0400

    Updated solution code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
 // fetch() & .then()
+// fetch() is an asynchronous function which we are using to make a GET request to get our food data from the JSON server (db.json)
 fetch("http://localhost:3000/foods")
 .then(response => response.json())
 .then(foods => {
-    
-    // Here is a for loop which can also be used to loop through the array.
-    // I've left the code for the for loop commented out, so you can compare it with the .forEach() array iterator seen on lines 85 - 87.
-    
-    // for(let index = 0; index < foods.length; index++){
-    //     addFoodImageToMenu(foods[index])
-    // }
-    
+
+    console.log("FETCH 1")
+
     foods.forEach(food => {
         addFoodImageToMenu(food)
     })
+    // addFoodDetails(foods[0])
 })
 
 fetch("http://localhost:3000/foods/1")
 .then(response => response.json())
 .then(food => {
+
+    console.log("FETCH 2")
+
     displayFoodDetails(food)
 })
 
+console.log("NOT A FETCH")
 
 function addFoodImageToMenu(food){
     const restaurantMenu = document.getElementById('restaurant-menu')
-    const image = document.createElement('img')
-    image.src = food.image
-    image.addEventListener('click', () => {
+    const foodImage = document.createElement('img')
+    foodImage.src = food.image
+    foodImage.addEventListener('click', () => {
         displayFoodDetails(food)
     })
-    restaurantMenu.appendChild(image)
+    restaurantMenu.appendChild(foodImage)
 }
 
 function displayFoodDetails(food){
-    const foodDetailImage = document.querySelector('.detail-image')
-    foodDetailImage.src = food.image
+    const foodImage = document.querySelector('.detail-image')
+    foodImage.src = food.image
 
-    const foodName = document.querySelector('.name') 
+    const foodName = document.querySelector('.name')
     foodName.textContent = food.name
 
-    const foodDescription = document.querySelector(`#description-display`)
+    const foodDescription = document.querySelector('#description-display')
     foodDescription.textContent = food.description
 }
